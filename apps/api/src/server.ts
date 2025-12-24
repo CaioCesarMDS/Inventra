7ce65fbc5db5c35d429470faec923d2653c2ffc9
CaioCesarMDS <caiocesarsts@gmail.com>
2025-12-24 17:57:14 -0300

    first commit

---
+++ apps/api/src/server.ts
@@ -0,0 +1,43 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import ScalarApiReference from "@scalar/fastify-api-reference";
+import { fastify } from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Stockify API",
+      description: "API for inventory management",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(ScalarApiReference, {
+  routePrefix: "/docs",
+});
+
+const port = Number(process.env.PORT ?? 3000);
+
+app.listen({ port, host: "0.0.0.0" }).then(() => {
+  console.log(`🔥 HTTP server running on http://localhost:${port}`);
+  console.log(`📚 Docs available at http://localhost:${port}/docs`);
+});
